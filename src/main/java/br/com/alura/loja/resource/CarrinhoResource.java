6c7019228283a271fbf0a195fc02e4d34c36ad36
Italo Gustavo Miranda Melo <italogmm@gmail.com>
2016-02-02 10:46:12 -0200

    Aula 11 - REST - ALURA

--- src/main/java/br/com/alura/loja/resource/CarrinhoResource.java
+++ src/main/java/br/com/alura/loja/resource/CarrinhoResource.java
@@ -6,16 +6,18 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import com.thoughtworks.xstream.XStream;
-
 import br.com.alura.loja.dao.CarrinhoDAO;
 import br.com.alura.loja.modelo.Carrinho;
+import br.com.alura.loja.modelo.Produto;
+
+import com.thoughtworks.xstream.XStream;
 
 @Path("carrinhos")
 public class CarrinhoResource {
@@ -50,6 +52,15 @@ public class CarrinhoResource {
 	@DELETE
 	public Response removeCarrinho(@PathParam("id") long id){
 		new CarrinhoDAO().remove(id);
+		return Response.ok().build();
+	}
+	
+	@Path("{id}/produtos/{produtoId}/quantidade")
+	@PUT
+	public Response alteraProduto(String conteudo, @PathParam("id") long id, @PathParam("produtoId") long produtoId){
+		Carrinho carrinho = new CarrinhoDAO().busca(id);
+		Produto produto = (Produto) new XStream().fromXML(conteudo);
+		carrinho.trocaQuantidade(produto);
 		return Response.ok().build();
 	}
 }
