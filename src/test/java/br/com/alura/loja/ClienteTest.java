fe357c7fc30bc595a8f868e95da6f0eed5cbe693
Italo Gustavo Miranda Melo <italogmm@gmail.com>
2016-02-02 09:02:26 -0200

    Aula 10 - REST - Alura

--- src/test/java/br/com/alura/loja/ClienteTest.java
+++ src/test/java/br/com/alura/loja/ClienteTest.java
@@ -1,5 +1,6 @@
 package br.com.alura.loja;
 
+import javax.ws.rs.InternalServerErrorException;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
@@ -8,15 +9,17 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.client.ClientConfig;
+import org.glassfish.jersey.filter.LoggingFilter;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import com.thoughtworks.xstream.XStream;
-
 import br.com.alura.loja.modelo.Carrinho;
 import br.com.alura.loja.modelo.Produto;
+
+import com.thoughtworks.xstream.XStream;
 
 public class ClienteTest {
 
@@ -27,7 +30,9 @@ public class ClienteTest {
 	@Before
 	public void startaServidor() {
 		this.server = Servidor.inicializaServidor();
-		this.client = ClientBuilder.newClient();
+		ClientConfig config = new ClientConfig();
+		config.register(new LoggingFilter());
+		this.client = ClientBuilder.newClient(config);
 		this.target = client.target("http://localhost:8080/");
 	}
 
@@ -47,9 +52,6 @@ public class ClienteTest {
 	@Test
 	public void testaQueSuportaNovosCarrinhos(){
 		
-		Client client = ClientBuilder.newClient();
-        WebTarget target = client.target("http://localhost:8080");
-        
         Carrinho carrinho = new Carrinho();
         carrinho.adiciona(new Produto(314L, "Tablet", 999, 1));
         carrinho.setRua("Rua Vergueiro");
@@ -63,5 +65,28 @@ public class ClienteTest {
         String location = response.getHeaderString("Location");
         String conteudo = client.target(location).request().get(String.class);
         Assert.assertTrue(conteudo.contains("Tablet"));
+	}
+	
+	@Test(expected = InternalServerErrorException.class)
+	public void testaDeleteCarrinho(){
+		
+		Carrinho carrinho = new Carrinho();
+        carrinho.adiciona(new Produto(314L, "Produto 1", 999, 1));
+        carrinho.setRua("Rua Vergueiro");
+        carrinho.setCidade("Goiânia");
+        String xml = carrinho.toXML();
+        
+        Entity<String> entity = Entity.entity(xml, MediaType.APPLICATION_XML);
+
+        Response response = target.path("/carrinhos").request().post(entity);
+        Assert.assertEquals(201, response.getStatus());
+        String location = response.getHeaderString("Location");
+        String conteudo = client.target(location).request().get(String.class);
+        
+        Carrinho carrinhoConsultado = (Carrinho) new XStream().fromXML(conteudo);
+        Response responseDelete = target.path("/carrinhos/" + carrinhoConsultado.getId()).request().delete();
+        Assert.assertEquals(200, responseDelete.getStatus());
+        
+        String conteudoAposDelete = client.target(location).request().get(String.class);
 	}
 }
